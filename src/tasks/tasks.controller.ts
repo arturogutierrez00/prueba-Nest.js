ad1a4b9e529f7998477812d9b2415dc11acfa16f
arturogutierrez00 <arturo.g@bidcom.com.ar>
2024-08-12 16:10:46 -0300

    3 commit

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put , Query, UsePipes, ValidationPipe} from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
+import { ApiTags } from "@nestjs/swagger";
 // import { updateTasks } from "./dto/update-task.dto";
 
 
@@ -15,34 +16,34 @@ export class TaskController{
     constructor( tasksService:TasksService){
         this.tasksService = tasksService;
     }
-
+    @ApiTags('tasks')
     @Get('/')
     getAllTasks(@Query() query: any){
         console.log(query)
       return this.tasksService.getTasks();
     }
-
+    @ApiTags('tasks')
     @Get('/:id')
     gettasks(@Param('id') id: string){
         return this.tasksService.getTask(parseInt(id));
     }
-
+    @ApiTags('tasks')
     @Put('')
     updateTasks(){
         return this.tasksService.updateTasks();
     }
-
+    @ApiTags('tasks')
     @Post('')
     @UsePipes( new ValidationPipe())
     createTasks( @Body() tasks: CreateTaskDto){
         return this.tasksService.createTask(tasks);
     }
-
+    @ApiTags('tasks')
     @Delete('')
     deleteTasks(){
         return this.tasksService.deleteTasks();
     }
-
+    @ApiTags('tasks')
     @Patch('')
     updateTasksStatus(){
         return this.tasksService.updateTasksStatus()
